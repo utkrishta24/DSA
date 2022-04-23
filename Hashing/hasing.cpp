615b36ef1d8a85456363b100d66a6af378c258df
Utkrishta Sinha <krishsinha2406@gmail.com>
2022-04-23 16:06:49 +0530

    add

--- Hashing/hasing.cpp
+++ Hashing/hasing.cpp
@@ -1,2 +1,15 @@
 //search insert delete : O(1) on avg
 //no duplicates allowed
+//not useful for finding closet value(AVL and Red Black), sorted data(AVL and Red Black), prefix searching(trie)
+
+//Application:
+//dictionaries
+//databox indexing
+//cryptography
+//caches
+//symbol tables in complier/interpreter
+//routers
+//getting data from databases
+//etc.
+
+//Direct address table:
