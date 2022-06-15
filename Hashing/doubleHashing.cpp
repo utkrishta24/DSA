4899a2edf86468f5f6d029703c3d3f260d07c9b9
Utkrishta Sinha <krishsinha2406@gmail.com>
2022-06-15 18:57:44 +0530

    main

--- Hashing/doubleHashing.cpp
+++ Hashing/doubleHashing.cpp
@@ -4,4 +4,21 @@
     //distributes keys more uniformly than linear probing and quadratic hshing
     //no clustering
     //why h2(key) and m be rel. prime?
-    // 
+    // always finds a free slot if there is a free slot 
+    
+//algo
+    // void doubleHasingInsert(key){
+    //     if (table if full)
+    //         return error
+    //     probe= h1(key), offset=h2(key)
+    //     while(table[probe] is occupied)
+    //         probe = (probe + offset )
+    //     table[probe] = key
+    // }
+    //linear probe offset=1//
+
+    //performance analysis of search(unsuccessful)
+    //alpha = n/m (should be <=1)
+    // assumption: every probe seq looks ar a random location
+    //(1-alpha) fraction of table is empty
+    //expected no of probes required = 1/(1-alpha)
---
+++ Hashing/openAddImp.cpp
@@ -0,0 +1,55 @@
+// MyHash mh(7);
+// mh.insert(49)
+// mh.insert(56)
+// mh.insert(72)
+// if(mh.search(56)==true)
+// print(yes)
+// else
+// print(no)
+// mh.erase(56)
+// if(mh.search(56)==true)
+// print(yes)
+// else
+// print(no)
+
+// 49 56 72 -1 -1 -1
+// erase(56)
+// 49 -2 72 -1 -1 -1
+
+struct MyHash
+{
+    int *arr;
+    int cap, size;
+    MyHash(int c)
+    {
+        cap = c;
+        size = 0;
+        for (int i = 0; i < cap; i++)
+            arr[i] = -1;
+    }
+    int hash(int key)
+    {
+        return key % cap;
+    }
+    bool search(int key)
+    {
+        int h = hash(key);
+        int i = h;
+        while (arr[i] != -1)
+        {
+            if (arr[i] == key)
+                return true;
+            i = (i + 1) % cap;
+            if (i == h)
+                return false;
+        }
+        return false;
+    }
+    bool insert(int key)
+    {
+        `
+    }
+    bool erase(int key)
+    {
+    }
+};
