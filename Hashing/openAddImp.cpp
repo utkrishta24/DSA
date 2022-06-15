21c42467254e356d85f0c7755966d0af87503b34
Utkrishta Sinha <krishsinha2406@gmail.com>
2022-06-15 19:06:15 +0530

    main

--- Hashing/openAddImp.cpp
+++ Hashing/openAddImp.cpp
@@ -20,6 +20,7 @@ struct MyHash
 {
     int *arr;
     int cap, size;
+
     MyHash(int c)
     {
         cap = c;
@@ -27,10 +28,12 @@ struct MyHash
         for (int i = 0; i < cap; i++)
             arr[i] = -1;
     }
+
     int hash(int key)
     {
         return key % cap;
     }
+
     bool search(int key)
     {
         int h = hash(key);
@@ -45,11 +48,41 @@ struct MyHash
         }
         return false;
     }
+
     bool insert(int key)
     {
-        `
+        if (size == cap)
+            return false;
+        int i = hash(key);
+        while (arr[i] != -1 && arr[i] != -2 && arr[i] != key)
+            i = (i + 1) % cap;
+        if (arr[i] == key)
+        {
+            return false;
+        }
+        else
+        {
+            arr[i] = key;
+            size++;
+            return true;
+        }
     }
+
     bool erase(int key)
     {
+        int h = hash(key);
+        int i = h;
+        while (arr[i] != -1)
+        {
+            if (arr[i] == key)
+            {
+                arr[i] = -2;
+                return true;
+            }
+            i = (i + 1) % cap;
+            if (i == h)
+                return false;
+        }
+        return false;
     }
 };
