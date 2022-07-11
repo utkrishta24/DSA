15dc6996d0a1c3170ef5ca4cc62d36da88bb06c3
Utkrishta Sinha <krishsinha2406@gmail.com>
2022-07-11 17:38:38 +0530

    main

--- Searching/searchInfiniteArray.cpp
+++ Searching/searchInfiniteArray.cpp
@@ -1,11 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int searchInInfinite()
-
-
-int main()
+// naive:
+//O(position)
+//AS: O(1)
+int searchInInfinite(  int arr[], int x )
 {
-    
- return 0;
+    int i=0;
+    while (true)
+    {
+        if (arr[i]==x) return i;
+        if (arr[i]>x) return -1;
+    }
+}
+
+// efficient:
+//0(log(position))
+int searchInInfinite(int arr[], int x)
+{
+    if(arr[0]==x) return 0;
+    int i=1;
+    while (arr[i]<x)
+        i=i*2;
+    if(arr[i]==x) return i;
+    return binarySearch(arr,i/2+1,i-1);
 }
