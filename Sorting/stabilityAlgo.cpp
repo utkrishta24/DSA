9cbdbf30d89cbc6ac6e3a291e2d6351ba8b0e31d
Utkrishta Sinha <krishsinha2406@gmail.com>
2022-07-17 00:29:47 +0530

    main

--- Sorting/stabilityAlgo.cpp
+++ Sorting/stabilityAlgo.cpp
@@ -1,1 +1,16 @@
-f
+/*
+
+arr[]={("Anil",50),("Ayan",80),("Piyush",50),("Ramesh",80)}
+
+stable sorting:
+arr[]={("Anil",50),("Piyush",50),("Ayan",80),("Ramesh",80)}
+
+unstable sorting:
+arr[]={("Piyush",50),("Anil",50),("Ayan",80),("Ramesh",80)}
+
+Stable: Bubble, Inseretion, Merge,...
+Unstable: Selection, Quick, Heap,...
+
+
+
+*/
