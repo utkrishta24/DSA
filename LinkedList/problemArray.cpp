6362498d14cee49969091c503cfb72958aaf2981
Utkrishta Sinha <krishsinha2406@gmail.com>
2022-07-16 11:39:07 +0530

    main

---
+++ LinkedList/problemArray.cpp
@@ -0,0 +1,4 @@
+// 1. either size is fixed or pre-allocated (in both fixed and variable sized array) OR the worst insertion at the end is 0(n).
+// 2. insertion at middle or beginning is costly.
+// 3. deletion at middle or beginning is costly.
+// 4. implementation of data structures like queue and deque is complex with arrays.
