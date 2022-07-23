b42ce48129f76282f7802d6d87235fd7e36ba234
Utkrishta Sinha <krishsinha2406@gmail.com>
2022-07-23 17:13:46 +0530

    main

--- Sorting/bucketSort.cpp
+++ Sorting/bucketSort.cpp
@@ -5,32 +5,49 @@ using namespace std;
 
 // consider another situation where we have floating point numbers uniformly d istributed in range 0.0 to 1.0(excluded).
 
-void bucketSort(int arr[], int n)
-{
-    int count[k];
-    for (int i = 0; i < k; i++)
-        count[i] = 0;
-    for (int i = 0; i < n; i++)
-        count[arr[i]]++;
+// linear time to scatter
+// linear  time to sort buckets
+// linear time to join sorted buckets
 
-    int index = 0;
-    for (int i = 0; i < k; i++)
+// might work bad if not uniform data
+// O(n2) worst case
+
+void bucketSort(int arr[], int n, int k)
+{ // find max value
+    int max_val = arr[0];
+    for (int i = 0; i < n; i++)
+        max_val = max(max_val, arr[i]);
+    max_val++;
+    vector<int> bkt[k];
+    // fill buckets
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < count[i]; j++)
-        {
-            arr[index] = i;
-            index++;
-        }
+        int bi = (k * arr[i]) / max_val;
+        bkt[bi].push_back(arr[i]);
     }
+    // sort buckets
+    for (int i = 0; i < k; i++)
+        sort(bkt[i].begin(), bkt[i].end());
+    int index = 0;
+    // join buckets
+    for (int i = 0; i < k; i++)
+        for (int j = 0; j < bkt[i].size(); j++)
+            arr[index++] = bkt[i][j];
 }
 
 int main()
 {
-    int arr[] = {1, 4, 4, 1, 0, 1};
-    int k = 5;
-    int n = 6;
-    bucketSort(arr, n);
+    int arr[] = {20, 80, 10, 85, 75, 99, 18};
+    int k = 5; // buckets
+    int n = 7;
+    bucketSort(arr, n, k);
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
     return 0;
 }
+
+// Assuming k~=n
+// Best case: data is uniformly distributed O(n)
+// Worst case: all items go into single bucket:
+// If we use insertion sort to sort the individual buckets, then O(n2).
+// If we use O(nlogn) algo sort to sort the individual buckets, then O(nlogn).
