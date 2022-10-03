4f2593ad562681e4aacee7797a84c3f25b90fe56
utkrishta24 <krishsinha2406@gmail.com>
2022-10-03 19:12:25 +0530

    commit

--- Strings/naivePatternSearch.cpp
+++ Strings/naivePatternSearch.cpp
@@ -1,28 +1,30 @@
-#include<bits/stdc++.h>
+// Naive O((n-m+1)*m)
+// AS : O(1)
+
+#include <bits/stdc++.h>
 using namespace std;
 
-void pattSearching(string &txt, string &pat){
-    int m=pat.length();
-    int n=txt.length();
-    for (int i = 0; i < n-m; i++)
+void pattSearching(string &txt, string &pat)
+{
+    int m = pat.length();
+    int n = txt.length();
+    for (int i = 0; i <= n - m; i++)
     {
         int j;
-        for ( j = 0; j < m; j++)
+        for (j = 0; j < m; j++)
         {
-            if(pat[j]!=txt[i+j])
+            if (pat[j] != txt[i + j])
                 break;
         }
-        if(j==m)
-            cout<<i<<" ";
-        
+        if (j == m)
+            cout << i << " ";
     }
-    
 }
 
 int main()
 {
     string txt = "geeksforgeeks";
     string pat = "eek";
-    pattSearching(txt,pat);
- return 0;
+    pattSearching(txt, pat);
+    return 0;
 }
