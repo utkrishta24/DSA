9362b103ebfc8b7086345909767cebd0497eb960
utkrishta24 <krishsinha2406@gmail.com>
2023-02-08 10:04:08 +0530

    main

--- Arrays/maxDiff.cpp
+++ Arrays/maxDiff.cpp
@@ -1,8 +1,8 @@
 // Maximum value of arr[j]-arr[i] where j>i
 
-//naive 0(n^2)
-// #include<bits/stdc++.h>
-// using namespace std;
+// naive 0(n^2)
+//  #include<bits/stdc++.h>
+//  using namespace std;
 
 // void diff(int arr[], int n){
 //     int maxDiff=arr[1]-arr[0];
@@ -12,27 +12,29 @@
 //     cout<<maxDiff;
 // }
 
-//efficient 0(n) AS:0(1)
-#include<bits/stdc++.h>
+// efficient 0(n) AS:0(1)
+#include <bits/stdc++.h>
 using namespace std;
 
-void diff(int arr[], int n){
-    int maxDiff=arr[1]-arr[0];
-    int minVal=arr[0];
-    for(int j=0;j<n-1;j++)
+void diff(int arr[], int n)
+{
+    int maxDiff = arr[1] - arr[0];
+    int minVal = arr[0];
+    for (int j = 0; j < n; j++)
     {
-        maxDiff=max(maxDiff,arr[j]-minVal);
-        minVal= min(minVal, arr[j]);
+        maxDiff = max(maxDiff, arr[j] - minVal);
+        minVal = min(minVal, arr[j]);
     }
-    cout<<maxDiff;
+    cout << maxDiff;
 }
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++)
-    cin>>arr[i];
-    diff(arr,n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    diff(arr, n);
     return 0;
 }
